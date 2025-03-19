2025b7e31a685a30a19b972243ffc27843b21c7a
JR Concepcion <137083455+JRCon1@users.noreply.github.com>
2025-03-18 20:22:34 -0400

    Update workingfile1.py

--- workingfile1.py
+++ workingfile1.py
@@ -1800,4 +1800,5 @@ def update_ic_analysis(ticker, expiry, short_call, long_call, short_put, long_pu
 # Run the App
 # ---------------------------
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run_server(host='0.0.0.0', port=port, debug=True)
